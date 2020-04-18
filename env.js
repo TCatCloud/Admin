67f34e1aadd0da58e4ab239d33686b54729cf9a6
Moexin <moexin@foxmail.com>
2020-04-18 12:30:22 +0800

    Update env.js

--- env.js
+++ env.js
@@ -10,5 +10,5 @@ window.v2board = {
     color: 'default'
   },
   // 背景
-  background_url: 'https://cdn.jsdelivr.net/gh/MoexinCDN/Picture@master/CatBG1.jpg'
+  background_url: 'https://moexin.coding.net/p/Picture/d/Picture/git/raw/master/CatBG1.jpg'
 }
