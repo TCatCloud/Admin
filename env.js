46139780b22d1e770bd46d69305812d5796e8910
Moexin <moexin@qq.com>
2020-12-11 17:54:33 +0800

    Update

--- env.js
+++ env.js
@@ -10,5 +10,5 @@ window.settings = {
     color: 'default'
   },
   // 背景
-  background_url: 'https://cdn.jsdelivr.net/gh/MoexinCDN/TCat@master/TCatBG1.jpg'
+  background_url: 'https://cdn.jsdelivr.net/gh/TCatCloud/Static@master/TCatBG1.jpg'
 }
